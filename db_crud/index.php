bfec182f7b4b86f4bafccb19f3be7ce15ac79dd9
Fajrul Fahmi <95855377+fajrulfahmi@users.noreply.github.com>
2024-03-07 05:57:18 +0700

    Add files via upload

---
+++ db_crud/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Data Barang</title>
+</head>
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+<body class="bg-secondary">
+	<div class="container col-md-8 mt-5">
+		<!-- <h1 class="text-center mb-4 text-light">Penyajian Data Barang</h1> -->
+		<div class="card border-0">
+			<div class="card-header bg-dark text-white ">
+				DATA BARANG <a href="tambah.php" class="btn btn-sm btn-primary float-right">Tambah</a>
+			</div>
+			<div class="card-body">
+				<table class="table table-bordered">
+					<thead>
+						<tr>
+							<th>No</th>
+							<th>Nama Barang</th>
+							<th>Harga</th>
+							<th>Deskripsi</th>
+							<th>Aksi</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						include('koneksi.php'); //memanggil file koneksi
+						$datas = mysqli_query($koneksi, "select * from barang") or die(mysqli_error($koneksi));
+						//script untuk menampilkan data barang
+
+						$no = 1; //untuk pengurutan nomor
+
+						//melakukan perulangan
+						while ($row = mysqli_fetch_assoc($datas)) {
+						?>
+
+							<tr>
+								<td><?= $no; ?></td>
+								<td><?= $row['nama']; //untuk menampilkan nama 
+									?></td>
+								<td>Rp <?= $row['harga']; ?></td>
+								<td><?= $row['deskripsi']; ?></td>
+								<td>
+									<a href="edit.php?id=<?= $row['id']; ?>" class="btn btn-sm btn-warning">Edit</a>
+									<a href="hapus.php?id=<?= $row['id']; ?>" class="btn btn-sm btn-danger" onclick="return confirm('anda yakin ingin hapus?');">Hapus</a>
+								</td>
+							</tr>
+
+						<?php $no++;
+						} ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
